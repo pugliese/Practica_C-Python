7f8b04a9f33dc06b8ad59defdb27aa6235a9324f
pugliese <facu_pugliese@hotmail.com>
2018-05-01 21:36:51 -0300

    Arreglados errores basicos de compilación

--- morse/morse_pot.c
+++ morse/morse_pot.c
@@ -1,4 +1,4 @@
-#include morse_pot.h
+#include "morse_pot.h"
 
 float forces(float *x, long int* pairs, long int npairs, float alpha,float D, float req, float rcut, float *force){
   float energy = 0;
@@ -7,8 +7,8 @@ float forces(float *x, long int* pairs, long int npairs, float alpha,float D, fl
 
   for(int l=0;l<npairs;l++){
     float delta_r[3];
-    int i = npairs[2*l];
-    int j = npairs[2*l+1];
+    int i = pairs[2*l];
+    int j = pairs[2*l+1];
 
     for(int k=0;k<3;k++){
       delta_r[3] = x[3*i+k]-x[3*j+k];
@@ -16,7 +16,7 @@ float forces(float *x, long int* pairs, long int npairs, float alpha,float D, fl
 
     float r = 0;
     for(int k=0;k<3;k++){
-      r = r + delta_r*delta_r;
+      r = r + delta_r[k]*delta_r[k];
     }
     r = sqrt(r);
 
