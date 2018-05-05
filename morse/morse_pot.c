aa683e12442c02675acc857e696aecdcdfc525f7
pugliese <facu_pugliese@hotmail.com>
2018-05-05 16:40:49 -0300

    Correcciones de estilo

--- morse/morse_pot.c
+++ morse/morse_pot.c
@@ -1,33 +1,35 @@
 #include "morse_pot.h"
 
-float forces(float *x, long int* pairs, long int npairs, float alpha,float D, float req, float rcut, float *force){
+float forces(float *x, long int* pairs, long int npairs,
+  float alpha,float D, float req, float rcut, float *force){
+
   float energy = 0;
   float energy_cut = (1-exp(-alpha*(rcut-req)));
   energy_cut = D*energy_cut*energy_cut;
 
-  for(int l=0;l<npairs;l++){
+  for (int l = 0; l < npairs; l++) {
     float delta_r[3];
     int i = pairs[2*l];
     int j = pairs[2*l+1];
 
-    for(int k=0;k<3;k++){
+    for (int k = 0; k < 3; k++) {
       delta_r[3] = x[3*i+k]-x[3*j+k];
     }
 
     float r = 0;
-    for(int k=0;k<3;k++){
+    for (int k = 0; k < 3; k++) {
       r = r + delta_r[k]*delta_r[k];
     }
     r = sqrt(r);
 
-    if (r<rcut){
+    if (r<rcut) {
       float mexp = exp(-alpha*(r-req));
       float m_force = -2*alpha*D*(1-mexp)*mexp/r;
       for(int k=0;k<3;k++){
         force[3*i+k] = force[3*i+k] + m_force*delta_r[k];
         force[3*j+k] = force[3*j+k] - m_force*delta_r[k];
       }
-      energy = energy + D*(1-2*mexp+mexp*mexp) - energy_cut;
+      energy = energy + D*(1-mexp)*(1-mexp) - energy_cut;
     }
   }
 
