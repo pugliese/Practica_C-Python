577a49980431c28ddb44aca16a020365542eff8b
pugliese <facu_pugliese@hotmail.com>
2018-06-13 17:14:37 -0300

    Agregadas comparaciones de velocidad para potencial LJ

---
+++ LJ/speed.sh
@@ -0,0 +1,18 @@
+Os=(0 1 2 3 fast)
+fs=(1 2 3)
+filename="data.txt"
+Niter=1
+Npart=216
+Nstat=1000
+
+rm $filename
+echo "#" ${fs[@]} >> $filename
+echo "#" ${Os[@]} >> $filename
+echo "#" $Npart >> $filename
+for O in ${Os[@]}
+do
+  #make rmv
+  make clean >/dev/null
+  make O=$O >/dev/null
+  python3 speed_test_python.py $Niter $Npart $Nstat $filename ${fs[@]}
+done
---
+++ LJ/speed_test_python.py
@@ -0,0 +1,122 @@
+import itertools as it
+import numpy as np
+import matplotlib.pylab as plt
+import sys
+from datetime import datetime
+import ctypes as ct
+
+speed = ct.CDLL('./speed_test.so')
+(speed.forces1).argtypes = [ct.c_voidp, ct.c_voidp, ct.c_longlong, ct.c_float,
+                       ct.c_float, ct.c_float, ct.c_voidp]
+(speed.forces1).restype = ct.c_float
+(speed.forces2).argtypes = [ct.c_voidp, ct.c_voidp, ct.c_longlong, ct.c_float,
+                       ct.c_float, ct.c_float, ct.c_voidp]
+(speed.forces2).restype = ct.c_float
+(speed.forces3).argtypes = [ct.c_voidp, ct.c_voidp, ct.c_longlong, ct.c_float,
+                       ct.c_float, ct.c_float, ct.c_voidp]
+(speed.forces3).restype = ct.c_float
+
+def particulas(Npart):
+    x = np.zeros((Npart,3), dtype=np.float32)
+    n3 = int(np.ceil(Npart**(1.0/3)))
+    i = 0
+    for p in it.product(range(n3),range(n3),range(n3)):
+        if Npart <= i:
+            break
+        x[i,:] = np.array(p)*5.0/n3
+        i += 1
+    return x
+
+
+
+def tiempo_iter(fs,Niter,Npart,Nstat=1):
+    x = particulas(Npart)
+    xp = x.ctypes.data_as(ct.c_voidp)
+    pairs = np.array(list(it.combinations(range(len(x)), 2)), dtype=np.int64)
+    pairsp = pairs.ctypes.data_as(ct.c_voidp)
+    dummy_forces = x.copy()
+    dummy_forcesp = (dummy_forces).ctypes.data_as(ct.c_voidp)
+    prom = np.zeros(len(Niter))
+    var = np.zeros(len(Niter))
+    f = lambda : eval("speed.forces"+str(fs))(xp,pairsp,len(pairs),1,1,1,100,dummy_forcesp)
+    for n in range(len(Niter)):
+        print(Niter[n])
+        for i in range(Nstat):
+            t0 = datetime.now()
+            for j in range(Niter[n]):
+                f()
+            t1 = datetime.now()
+            prom[n] += (t1-t0).total_seconds()/Nstat
+            var[n] += (t1-t0).total_seconds()**2/Nstat
+    var -= prom*prom
+    return prom, var
+
+
+def tiempo_part(fs,Niter,Npart,Nstat=1):
+    prom = np.zeros(len(Npart))
+    var = np.zeros(len(Npart))
+    for n in range(len(Npart)):
+        print(Npart[n])
+        x = particulas(Npart[n])
+        xp = x.ctypes.data_as(ct.c_voidp)
+        pairs = np.array(list(it.combinations(range(len(x)), 2)), dtype=np.int64)
+        pairsp = pairs.ctypes.data_as(ct.c_voidp)
+        dummy_forces = x.copy()
+        dummy_forcesp = (dummy_forces).ctypes.data_as(ct.c_voidp)
+        f = lambda : eval("speed.forces"+str(fs))(xp,pairsp,len(pairs),1,1,1,100,dummy_forcesp)
+        for i in range(Nstat):
+            t0 = datetime.now()
+            for j in range(Niter):
+                f()
+            t1 = datetime.now()
+            prom[n] += (t1-t0).total_seconds()/Nstat
+            var[n] += (t1-t0).total_seconds()**2/Nstat
+    var -= prom*prom
+    return prom,var
+
+def comp(Niter,Npart,Nstat=1,fs=range(1,9)):
+    prom = np.zeros(len(fs))
+    x = particulas(Npart)
+    xp = x.ctypes.data_as(ct.c_voidp)
+    pairs = np.array(list(it.combinations(range(len(x)), 2)), dtype=np.int64)
+    pairsp = pairs.ctypes.data_as(ct.c_voidp)
+    dummy_forces = x.copy()
+    dummy_forcesp = (dummy_forces).ctypes.data_as(ct.c_voidp)
+    for j in range(len(fs)):
+        f = lambda : eval("speed.forces"+str(fs[j]))(xp,pairsp,len(pairs),1,1,100,dummy_forcesp)
+        #print("forces"+str(fs[j]))
+        for i in range(Nstat):
+            t0 = datetime.now()
+            for k in range(Niter):
+                f()
+            t1 = datetime.now()
+            prom[j] += (t1-t0).total_seconds()/Nstat
+    return prom
+
+def graf_barras(T):
+    #plt.title("Niter="+str(Niter)+" | Npart="+str(Npart)+" | Nstat="+str(Nstat))
+    plt.xlabel("Implementacion")
+    plt.ylabel("Tiempo [s]")
+    plt.bar(range(1,len(T)+1),T)
+    plt.show()
+
+if (1<len(sys.argv)):
+    Niter = int(sys.argv[1])
+    Npart = int(sys.argv[2])
+    nargs = len(sys.argv)
+    Nstat = int(sys.argv[3])
+    fs = range(1,9)
+    filename = sys.argv[4]
+    if(nargs>5):
+        fs = []
+        for i in range(5,nargs):
+            fs.append(int(sys.argv[i]))
+    formato = "%f"
+    for i in range(len(fs)-1):
+        formato = formato + ", %f"
+    formato += "\n"
+    file = open(filename, 'a')
+    T = comp(Niter,Npart,Nstat,fs)
+    #np.savetxt(file,T,newline=",")
+    file.write(formato %tuple(T))
+    file.close()
