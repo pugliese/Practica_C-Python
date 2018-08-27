c3578dcbbc912bbee8d772a0afda2cc68da0919a
Pablo Alcain <pabloalcain@gmail.com>
2018-08-27 18:03:36 -0300

    Actualizacion de formato de data

--- lammps_test/leer.py
+++ lammps_test/leer.py
@@ -5,21 +5,36 @@ import sys
 if (len(sys.argv)==0):
   print("Error: Falta nombre del archivo")
 else:
-  filename = sys.argv[1]
-  file = open(filename,"r")
-  line = file.readline()
-  Ecin = []
-  while (line!=""):
-    Ecin.append(0)
+  if (sys.argv[1]=='a'):
+    filename = sys.argv[2]
+    file = open(filename,"r")
+    line = file.readline()
+    Ecin = []
+    while (line!=""):
+      Ecin.append(0)
     while (line!="ITEM: ATOMS type x y z vx vy vz \n"):
       line = file.readline()
-    line = file.readline()
+      line = file.readline()
     while (line!="ITEM: TIMESTEP\n" and line!=""):
       data = line.split(" ")
       Ecin[-1] += float(data[-2])**2 + float(data[-3])**2 + float(data[-4])**2
       line = file.readline()
     Ecin[-1] /= 2
-  T = np.array(Ecin)/(3*4000)
-  print(T)
-  plt.plot(T, ".--")
-  plt.show()
+    T = np.array(Ecin)/(3*4000)
+    print(T)
+    plt.plot(T, ".--")
+    plt.show()
+
+  if (sys.argv[1]=='t'):
+    data = np.loadtxt(sys.argv[2], delimiter = ' ')
+    plt.figure(1)
+    plt.plot(data[:,0], '.--')
+    plt.ylabel('Temperatura')
+    plt.figure(2)
+    plt.plot(data[:,1]/2048, '.--')
+    plt.ylabel('Energia por particula')
+    plt.figure(3)
+    plt.xlabel('Temperatura')
+    plt.ylabel('Energia por particula')
+    plt.plot(data[20:,0],data[20:,1]/2048, '.')
+    plt.show()
