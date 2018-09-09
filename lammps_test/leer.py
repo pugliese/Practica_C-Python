e25cf2f0890828a049de8684333117e34172bef6
pugliese <facu_pugliese@hotmail.com>
2018-09-09 20:05:02 -0300

    Tests y analisis para pexmd completados

--- lammps_test/leer.py
+++ lammps_test/leer.py
@@ -2,6 +2,23 @@ import numpy as np
 import matplotlib.pylab as plt
 import sys
 
+def histograma_piola(data, x_lab, titulo, indice):
+    plt.figure(indice)
+    plt.grid()
+    plt.hist(data, 30)
+    plt.xlabel(x_lab)
+    plt.ylabel("Ocurrencias")
+    plt.title(titulo)
+    m = np.mean(data)
+    s = np.std(data)
+    plt.axvline(x=m, color="k")
+    plt.axvline(x=m+s, color="r")
+    plt.axvline(x=m-s, color="r")
+    orden = np.floor(np.log(s)/np.log(10))
+    m /= 10**orden
+    s /= 10**orden
+    plt.legend([r'$\mu$=%.1fE%d'%(m, orden) + '\n' + r'$\sigma$=%1.1fE%d'%(s,orden)])
+
 if (len(sys.argv)==0):
   print("Error: Falta nombre del archivo")
 else:
@@ -48,4 +65,90 @@ else:
     m, b = np.polyfit(T, E, 1)
     plt.plot(np.sort(T), np.sort(T)*m+b, "r-")
     print(m, b)
+    plt.show()
+
+  if (sys.argv[1]=='t2'):
+    n_skip = 0
+    dataFile = open(sys.argv[2])
+    line = dataFile.readline()
+    params = line.split(" ")
+    Npart, L, Nterm, Nportemp = int(params[1]), float(params[2]), int(params[3]), int(params[4])
+    line = dataFile.readline()
+    params = line.split(" ")
+    T = np.array([float(params[i]) for i in range(1,len(params))])
+    if (len(sys.argv)==4):
+      n_skip = int(sys.argv[3])
+    data = np.loadtxt(sys.argv[2], delimiter = ' ')
+    plt.grid()
+    E = data[Nterm:, 1]
+    mean_E = [np.mean(E[i*Nportemp+n_skip:(i+1)*Nportemp]) for i in range(len(T))]
+    var_E = [np.std(E[i*Nportemp+n_skip:(i+1)*Nportemp]) for i in range(len(T))]
+    plt.errorbar(T, mean_E, var_E, fmt="b.")
+    m, b = np.polyfit(T, mean_E, 1)
+    plt.plot(np.sort(T), np.sort(T)*m+b, "r-")
+    plt.axis([0,11,-30000,-18000])
+    print(m, b)
+    plt.show()
+
+  if (sys.argv[1]=='h'):
+    n_skip = 0
+    dataFile = open(sys.argv[2])
+    line = dataFile.readline()
+    params = line.split(" ")
+    Npart, L, Nterm, Nportemp = int(params[1]), float(params[2]), int(params[3]), int(params[4])
+    line = dataFile.readline()
+    params = line.split(" ")
+    T = np.array([float(params[i]) for i in range(1,len(params))])
+    if (len(sys.argv)==4):
+      n_skip = int(sys.argv[3])
+    data = np.loadtxt(sys.argv[2], delimiter = ' ')
+    T2 = data[Nterm:, 0]
+    E = data[Nterm:, 1]
+    E_sep = [np.array(E[i*Nportemp+n_skip:(i+1)*Nportemp]) for i in range(len(T))]
+    T_sep = [np.array(T2[i*Nportemp+n_skip:(i+1)*Nportemp]) for i in range(len(T))]
+    med = len(T)//2
+    histograma_piola(E_sep[0], "Energia", "T="+str(T[0]), 1)
+    histograma_piola(E_sep[med], "Energia", "T="+str(T[med]), 2)
+    histograma_piola(E_sep[-1], "Energia", "T="+str(T[-1]), 3)
+    histograma_piola(T_sep[0], "2Ecin/3N", "T="+str(T[0]), 4)
+    histograma_piola(T_sep[med], "2Ecin/3N", "T="+str(T[med]), 5)
+    histograma_piola(T_sep[-1], "2Ecin/3N", "T="+str(T[-1]), 6)
+    """
+    plt.figure(1)
+    plt.grid()
+    plt.hist(E_sep[0], 30)
+    plt.xlabel("Energía")
+    plt.ylabel("Ocurrencias")
+    plt.title("T="+str(T[0]))
+    plt.figure(2)
+    plt.grid()
+    plt.hist(E_sep[-1], 30)
+    plt.xlabel("Energía")
+    plt.ylabel("Ocurrencias")
+    plt.title("T="+str(T[-1]))
+    plt.figure(3)
+    plt.grid()
+    plt.hist(E_sep[len(T)//2], 30)
+    plt.xlabel("Energía")
+    plt.ylabel("Ocurrencias")
+    plt.title("T="+str(T[len(T)//2]))
+    plt.figure(4)
+    plt.grid()
+    plt.hist(T_sep[0], 30)
+    plt.xlabel("2Ecin/3N")
+    plt.ylabel("Ocurrencias")
+    plt.title("T="+str(T[0]))
+    plt.figure(5)
+    plt.grid()
+    plt.hist(T_sep[-1], 30)
+    plt.xlabel("2Ecin/3N")
+    plt.ylabel("Ocurrencias")
+    plt.title("T="+str(T[-1]))
+    plt.figure(6)
+    plt.grid()
+    plt.hist(T_sep[len(T)//2], 30)
+    plt.xlabel("2Ecin/3N")
+    plt.ylabel("Ocurrencias")
+    plt.title("T="+str(T[len(T)//2]))
+    """
     plt.show()
--- pexmd_test/test_morse.py
+++ pexmd_test/test_morse.py
@@ -50,7 +50,7 @@ if (sys.argv[1]=="e"):
   # Vecinos
   vecinos = Neighbour.Neighbour()
   pares = vecinos.build_list(part.x, "None")
-  
+
   # Termalizamos
   Epot = np.zeros(N)
   Ecin = np.zeros(N)
@@ -64,9 +64,9 @@ if (sys.argv[1]=="e"):
     part.f, Epot[i] = morse.forces(part.x,part.v, pares)
 
     part.f, Epot[N-1] = morse.forces(part.x,part.v, pares)
-    
+
     print(part.f)
-    
+
     plt.plot(Epot,"-b")
     plt.plot(Ecin, "-r")
     plt.plot(Ecin+Epot, "-g")
@@ -98,11 +98,11 @@ if (sys.argv[1]=="t"):
   pares = vecinos.build_list(part.x, part.t)
   # Termostato
   therm = thermostat.Andersen(To, 0.05, 1)
-  
+
   Nportemp = int(N_samp*0.8/N_temp)
   Nterm = N_samp - Nportemp*N_temp
   t0 = datetime.now()
-  dataFile= open("data.txt","w")
+  dataFile= open("data.txt", "w")
   for i in range(Nterm):
     part.x, part.v = verlet.first_step(part.x, part.v, part.f)
     bx.wrap_boundary(part.x, part.v)
@@ -115,6 +115,65 @@ if (sys.argv[1]=="t"):
   for i in range(N_temp):
     therm.temperature = To - i * T_step
     print(i)
+    for j in range(Nportemp):
+      part.x, part.v = verlet.first_step(part.x, part.v, part.f)
+      bx.wrap_boundary(part.x, part.v)
+      part.f, Epot = morse.forces(part.x, part.v, pares)
+      part.x, part.v = verlet.last_step(part.x, part.v, part.f)
+      part.v = therm.step(part.v, part.mass)
+      Ecin = energia_cinetica(part.v)
+      dataFile.write("%f %f\n" %(2*Ecin/(3*Npart), Ecin+Epot))
+  dataFile.close()
+  t1 = datetime.now()
+  print((t1-t0).total_seconds())
+
+if (sys.argv[1]=="t2"):
+	# Inicializo parametros
+	# Temperaturas
+  temperaturas = np.linspace(10,1,19)
+  Npart = 512
+  N_samp = 100000
+  # Caja
+  L = 6.3496
+  bx = Box.Box([0, 0, 0], [L, L, L], "Fixed")
+  # Particulas
+  part = Particles.PointParticles(Npart)
+  pos = particulas(Npart,L)
+  part.x = pos
+  part.v = np.random.normal(0,np.sqrt(temperaturas[0]*1.5),(Npart,3))
+  part.mass = np.zeros((Npart,1), dtype=np.float32) + 1
+  # Interaccion
+  morse = morse.Morse(2.5, 1.0, 0.5, 1.0)
+  # Integrador
+  verlet = Integrator.VelVerlet(0.005)
+  # Vecinos
+  vecinos = Neighbour.Neighbour()
+  pares = vecinos.build_list(part.x, part.t)
+  # Termostato
+  therm = thermostat.Andersen(temperaturas[0], 0.05, 1)
+
+  N_temp = len(temperaturas)
+  Nportemp = int(N_samp*0.8/N_temp)
+  Nterm = N_samp - Nportemp*N_temp
+  t0 = datetime.now()
+  dataFile = open("data.txt", "w")
+  dataFile.write('# %d %f %d %d\n' %(Npart, L, Nterm, Nportemp))
+  formato = '#'
+  for t in temperaturas:
+      formato += ' ' + str(t)
+  dataFile.write(formato+"\n")
+  for i in range(Nterm):
+    part.x, part.v = verlet.first_step(part.x, part.v, part.f)
+    bx.wrap_boundary(part.x, part.v)
+    part.f, Epot = morse.forces(part.x, part.v, pares)
+    part.x, part.v = verlet.last_step(part.x, part.v, part.f)
+    part.v = therm.step(part.v, part.mass)
+    Ecin = energia_cinetica(part.v)
+    dataFile.write("%f %f\n" %(2*Ecin/(3*Npart), Ecin+Epot))
+  print("Terminada termalizacion: %d" %Nterm)
+  for Temp in temperaturas:
+    therm.temperature = Temp
+    print(Temp)
     for j in range(Nportemp):
       part.x, part.v = verlet.first_step(part.x, part.v, part.f)
       bx.wrap_boundary(part.x, part.v)
